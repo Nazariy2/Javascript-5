25c4db9ca07f2d2e39b96c773aa3e6cf3e4e41b5
Nazariy2 <nazrud23@gmail.com>
2024-08-03 14:26:38 +0200

    a

--- script.js
+++ script.js
@@ -93,3 +93,30 @@
 //     messageElement.textContent = message;
     
 // }
+
+// function numberMonth() {
+//     const month =document.getElementById('month').value.trim();
+//     let days;
+
+//     const messageElement = document.getElementById('message');
+
+
+//     switch(parseInt(month)) {
+//         case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+//             days = 31;
+//             break;
+//         case 4: case 6: case 9: case 11:
+//             days = 30;
+//             break;
+//         case 2:
+//             days = 28; 
+//             break;
+//         default:
+//              days = 'Невірний номер місяця';
+//     }
+//     with alert
+//     alert(days);
+//     without alert
+//     messageElement.textContent = days;
+
+// }
